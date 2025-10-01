686e0fcc0f8f38b4f5fc089c0c102d534fb126ac
AL3X-AFK <alexvvaldezv@gmail.com>
2025-10-01 10:53:45 +0200

    Inicio ejercicios ppl mezclados- acceso a datos

--- Sistemas de gestion empresarial/pagina/js.js
+++ Sistemas de gestion empresarial/pagina/js.js
@@ -1,5 +1,5 @@
 // alert("Hola DAM2, soy una pagina externa")
-
+/*
 //Mensajes en consola
 console.log("Soy el console.log")
 console.info("Soy el console.info")
@@ -34,7 +34,7 @@ console.log("La resta es: " + resta)
 const SALUDO = "Hola"
 console.log("Te voy a saludar: " + SALUDO)
 /*SALUDO = "Bye bye"
-console.log("Nos vemos: " + SALUDO)*/
+console.log("Nos vemos: " + SALUDO)
 
 //Probando los tipos, Numbre
 let tres = 3, tresString = "3"
@@ -48,6 +48,8 @@ const total = 142
 const personas = 6
 console.log("A cada persona le tocara pagar: " + (142/6)) 
 
+
+
 let ordenador, cascos, teclado
 ordenador = parseInt(prompt("Precio del ordenador: "))
 cascos = parseInt(prompt("Precio de los cascos: "))
@@ -57,3 +59,29 @@ let gasto =  (ordenador + cascos + teclado) - ((ordenador + cascos + teclado) * 
 let texto = ("Te gastas: " + gasto)
 document.getElementById("parrafo").innerHTML = texto
 //alert("Gasto " + gasto)
+
+//Probando las propiedades de Number
+let num1 = 333
+console.log("El tipo de la variable num1 es: " + typeof(num1))
+
+console.log("El tipo de la variable num1 en casting es: " + typeof(num1.toString()))
+
+let fruta = "piña"
+console.log("Piña no es un numero?: " + isNaN(fruta))
+console.log("Piña no es un numero?: " + isNaN(num1.toString()))
+
+console.log("Num1 con 5 decimales: " + num1.toFixed(5))
+*/
+
+
+let nombre = "Alex"
+console.log("Nombre: " + nombre)
+console.log(`Nombre: ${nombre}`)
+
+console.log("Longitud de tu nombre: " + nombre.length)
+
+console.log("Typeof de tu nombre: " + typeof(nombre))
+
+console.log("Nombre incluye 'le': " + nombre.includes("le"))
+
+console.log("Nombre recortado: " + nombre.slice(1,4))
