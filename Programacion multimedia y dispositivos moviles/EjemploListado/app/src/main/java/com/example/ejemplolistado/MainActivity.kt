4d1fdf31e07066a2c7c8ade5866d21f278550847
AL3X-AFK <alexvvaldezv@gmail.com>
2025-10-24 13:00:59 +0200

    App tareas Act

--- Programacion multimedia y dispositivos moviles/EjemploListado/app/src/main/java/com/example/ejemplolistado/MainActivity.kt
+++ Programacion multimedia y dispositivos moviles/EjemploListado/app/src/main/java/com/example/ejemplolistado/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.ejemplolistado
 
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -48,5 +53,51 @@ class MainActivity : AppCompatActivity() {
         val dialog = Dialog(this)
         dialog.setContentView(R.layout.dialog_task)
         val btnAddTask: Button = dialog.findViewById(R.id.btnAddTask)
+        val etTask: EditText = dialog.findViewById(R.id.etTask)
+        val rgCategories: RadioGroup = dialog.findViewById(R.id.rgCategories)
+
+        btnAddTask.setOnClickListener {
+            val currentTask = etTask.text.toString()
+            if (currentTask.isNotEmpty()){
+                val selectedId = rgCategories.checkedRadioButtonId
+                val selectedRadioButton: RadioButton = rgCategories.findViewById(selectedId)
+                val currentCategory: TaskCategory = when(selectedRadioButton.text){
+                    "Fultbol" -> TaskCategory.Futbol
+                    "Rugby" -> TaskCategory.Rugby
+                    "Tennis" -> TaskCategory.Tennis
+                    else -> TaskCategory.Other
+            }
+                tasks.add(Task(currentTask, currentCategory))
+                updateTasks()
+                dialog.hide()
+            }
+
+        }
+        dialog.show()
+    }
+
+    private fun initComponent(){
+        rvCategories = findViewById(R.id.rvcategories)
+        rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.fabAddTask)
+    }
+
+    private  fun initUi(){
+        categoriesAdapter = CategoriesAdapter(categories)
+        rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        rvCategories.adapter = categoriesAdapter
+
+        tasksAdapter = TasksAdapter(tasks)
+        rvTasks.layoutManager = LinearLayoutManager(this)
+        rvTasks.adapter = tasksAdapter
+    }
+
+    private fun onItemSelected(position: Int){
+        tasks[position].isSelected = !tasks[position].isSelected
+        updateTasks()
+    }
+
+    private fun updateTasks(){
+        tasksAdapter.notifyDataSetChanged()
     }
 }
--- Programacion multimedia y dispositivos moviles/EjemploListado/app/src/main/java/com/example/ejemplolistado/TasksViewHolder.kt
+++ Programacion multimedia y dispositivos moviles/EjemploListado/app/src/main/java/com/example/ejemplolistado/TasksViewHolder.kt
@@ -1,4 +1,14 @@
 package com.example.ejemplolistado
 
-class TasksViewHolder {
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TasksViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private val tvTask: TextView = view.findViewById(R.id.tvTask)
+    private val cbTask: TextView = view.findViewById(R.id.cbTask)
+
+    fun render(task: Task){
+        tvTask.text = task.name
+    }
 }
