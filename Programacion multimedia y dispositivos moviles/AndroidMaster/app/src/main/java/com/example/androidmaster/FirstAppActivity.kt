ca93c7b2d5da850d68af360efa00ca3d989e1a0c
AL3X-AFK <alexvvaldezv@gmail.com>
2025-10-30 22:06:03 +0100

    Repaso app

---
+++ Programacion multimedia y dispositivos moviles/AndroidMaster/app/src/main/java/com/example/androidmaster/FirstAppActivity.kt
@@ -0,0 +1,20 @@
+package com.example.androidmaster
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FirstAppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_first_app)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+    }
+}
