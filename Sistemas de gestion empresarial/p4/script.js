c1ff896708892f35bd9d5ef29938a65ef8960d12
AL3X-AFK <alexvvaldezv@gmail.com>
2025-10-31 10:09:33 +0100

    Calculadora / gestion

--- Sistemas de gestion empresarial/p4/script.js
+++ Sistemas de gestion empresarial/p4/script.js
@@ -1,15 +1,40 @@
+var titulo = document.getElementsByTagName("h1")[0]
+var p1 = document.getElementsByTagName("p")[0]
+var imagen = document.getElementsByTagName("img")[0]
+
+titulo.addEventListener("mouseover", cambiarColorFondo)
+p1.addEventListener("click", cambiarContenido)
+imagen.addEventListener("mouseleave", cambiarImagen)
+
 function cambiarColorFondo(){
-    let titulo = document.getElementsByTagName("h1")[0]
-    titulo.style.backgroundColor="white"
-    titulo.style.color="red"
+
+    if (titulo.style.backgroundColor=="white"){ 
+        titulo.style.backgroundColor="rgb(212, 3, 3)"
+        titulo.style.color="white"
+    }else{ 
+        titulo.style.backgroundColor="white"
+        titulo.style.color="red"
+
+    }
 }
 
 function cambiarContenido(){
-    let parrafo = document.getElementById("parrafo")
-    parrafo.textContent="Parrafo cambiado"
+
+    if (parrafo.textContent=="Parrafo cambiado" ) 
+        parrafo.textContent="Estado original"
+    else
+        parrafo.textContent="Parrafo cambiado"
 }
 
 function cambiarImagen(){
-    let imagen = document.getElementById("imagen")
-    imagen.src="https://preview.redd.it/anyone-else-prefer-night-city-during-the-day-v0-nlmwljmcos3c1.jpg?width=640&crop=smart&auto=webp&s=8949040e6d6391eccde929a11db3d5bc7d2ff390"
+    imagen.id="id_imagen"
+
+    if (imagen.src=="https://preview.redd.it/anyone-else-prefer-night-city-during-the-day-v0-nlmwljmcos3c1.jpg?width=640&crop=smart&auto=webp&s=8949040e6d6391eccde929a11db3d5bc7d2ff390")
+        imagen.src = "https://static.cdprojektred.com/cms.cdprojektred.com/16x9_big/872822c5e50dc71f345416098d29fc3ae5cd26c1-1280x720.jpg" 
+    else
+        imagen.src="https://preview.redd.it/anyone-else-prefer-night-city-during-the-day-v0-nlmwljmcos3c1.jpg?width=640&crop=smart&auto=webp&s=8949040e6d6391eccde929a11db3d5bc7d2ff390"
 }
+
+
+
+
