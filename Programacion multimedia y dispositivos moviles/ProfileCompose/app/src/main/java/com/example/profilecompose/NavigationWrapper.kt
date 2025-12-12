8ec79b3eae9f2d58a59ebac05f54f033519b1300
AL3X-AFK <alexvvaldezv@gmail.com>
2025-12-12 13:07:12 +0100

    Moviles

--- Programacion multimedia y dispositivos moviles/ProfileCompose/app/src/main/java/com/example/profilecompose/NavigationWrapper.kt
+++ Programacion multimedia y dispositivos moviles/ProfileCompose/app/src/main/java/com/example/profilecompose/NavigationWrapper.kt
@@ -4,33 +4,59 @@ import android.R.attr.name
 import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import androidx.navigation.toRoute
+import com.example.profilecompose.dataBase.DBHelper
 
 @Composable
 fun NavigationWrapper(){
     Log.i("ANTES", "Se crea la BBDD")
     val context = LocalContext.current
     val db = DBHelper(context, null)
-    db.addUser()
+    db.addUser("admin", "password")
 
 
     val navController = rememberNavController()
+    NavHost(navController  = navController, startDestination=NavRoutes.LOGIN) {
+        composable(NavRoutes.LOGIN) {
+            LoginScreen(
+                navigateToCheckAccess = { username, password ->
+                    navController.navigate("check_access/$username/$password")
+                }
+            )
+        }
 
-    NavHost(navController  = navController, startDestination=Login) {
-        composable<Login> {
-            LoginScreen{ navController.navigate(Searcher) }
+        composable(
+            route = NavRoutes.CHECK_ACCESS_WITH_ARGS,
+            arguments = ListOf(
+                navArgument("username"){type = NavType.StringType},
+                navArgument("password"){type = NavType.StringType}
+            )
+        ){ backEntry ->
+            LaunchedEffect (key1 = Unit){
+                val existe = db.isThereAnyone(username, password)
+                if(existe){
+                    navController.navigate(NavRoutes.SEARCHER){
+                        popUpTo(NavRoutes.LOGIN){ inclusice = false }
+                    }
+                } else {
+                    navController.navigateUp()
+                }
+            }
         }
-        composable<Searcher> {
-            SearcherScreen {name -> navController.navigate(Profile(name=name)) }
+
+        composable(NavRoutes.SEARCHER){
+            SearcherScreen { name -> navController.navigate(Profile(name = name)) }
         }
+
 
         composable<Profile> {backStackEntry ->
             val profile:Profile = backStackEntry.toRoute()
-            ProfileScreen(profile.name){name ->
-                navController.navigate(Detail(name = name))
+            ProfileScreen(profile.name){navController.navigate(Detail(name = profile.name))
             }
         }
 
--- Programacion multimedia y dispositivos moviles/ProfileCompose/app/src/main/java/com/example/profilecompose/dataBase/DBHelper.kt
+++ Programacion multimedia y dispositivos moviles/ProfileCompose/app/src/main/java/com/example/profilecompose/dataBase/DBHelper.kt
@@ -6,14 +6,17 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.compose.material3.Text
 
-class DBHelper (context: Context, factory: SQLiteDatabase.CursorFactory):
-    SQLiteOpenHelper(){
+class DBHelper(
+    context: Context,
+    factory: SQLiteDatabase.CursorFactory? = null
+) : SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+
     override fun onCreate(db: SQLiteDatabase?) {
         val createTableQuery = """
-            CREATE TABLE  $TABLE_NAME(
-            $ID_COL INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            $NAME_COL TEXT,
-            $PASSWORD_COL TEXT
+            CREATE TABLE $TABLE_NAME(
+                $ID_COL INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                $NAME_COL TEXT,
+                $PASSWORD_COL TEXT
             )
         """.trimIndent()
         db?.execSQL(createTableQuery)
@@ -24,21 +27,35 @@ class DBHelper (context: Context, factory: SQLiteDatabase.CursorFactory):
         onCreate(db)
     }
 
-    fun addUser(name: String, password: String){
+    fun addUser(name: String, password: String) {
         val values = ContentValues().apply {
             put(NAME_COL, name)
             put(PASSWORD_COL, password)
         }
 
-        writableDatabase.use { db->
+        writableDatabase.use { db ->
             db.insert(TABLE_NAME, null, values)
         }
     }
 
-    fun getCountOfUser(name: String?, password: String?): Boolean{
-        val cursor = readableDatabase.rawQuery("SELECT * FROM $TABLE_NAME", null)
-        val count = cursor.count
-        cursor.close()
-        return count
+    fun isThereAnyone(name: String, password: String): Boolean {
+        return readableDatabase.rawQuery(
+            "SELECT * FROM $TABLE_NAME WHERE $NAME_COL = ? AND $PASSWORD_COL = ?",
+            arrayOf(name, password)
+        ).use { it.count != 0 }
+    }
+
+    fun getCountOfUser(): Int {
+        return readableDatabase.rawQuery("SELECT * FROM $TABLE_NAME", null).use { it.count }
+    }
+
+    companion object {
+        private const val DATABASE_NAME = "AppProfile"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "Users"
+        private const val ID_COL = "id"
+        private const val NAME_COL = "username"
+        private const val PASSWORD_COL = "password"
     }
 }
+
